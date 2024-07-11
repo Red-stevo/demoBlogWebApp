40756c196e951b62fe68c66947961bdcb05923cf
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-07-11 13:43:59 +0300

    Commit message >>>>> to be continued <<<<<

--- demoblogwebapp/src/Redux/Reducers/PostReducer.js
+++ demoblogwebapp/src/Redux/Reducers/PostReducer.js
@@ -4,9 +4,8 @@ import {ActionTypes} from "../ActionTypes/ActionTypes.js";
 const initialState = [];
 
 export const storePostState = (state = initialState, {type, payload}) => {
-    if(type === ActionTypes.FETCH_POST){
-            return [...state, ...payload];
-    }else {
+    if(type === ActionTypes.FETCH_POST)
+        return [...state, ...payload];
+    else
         return state;
-    }
 }
