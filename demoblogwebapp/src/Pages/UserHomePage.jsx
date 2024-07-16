a3a7ac596641b5ba66b8a157c88079c31d4207ee
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-07-16 17:27:12 +0300

    Commit message >>>>> add the comments counts <<<<<

--- demoblogwebapp/src/Pages/UserHomePage.jsx
+++ demoblogwebapp/src/Pages/UserHomePage.jsx
@@ -1,8 +1,9 @@
 import {useDispatch, useSelector} from "react-redux";
 import {useEffect} from "react";
 import {fetchPost} from "../Redux/Actions/Actions.js";
-import {Card, Spinner} from "react-bootstrap";
+import {Card, CardLink, Spinner} from "react-bootstrap";
 import "./../Styling/UserHomePage.css";
+import {LiaComment} from "react-icons/lia";
 ;
 const  UserHomePage = () => {
     const posts = useSelector(state => state.Posts);
@@ -21,14 +22,18 @@ const  UserHomePage = () => {
 
     return(
        <div id={"posts"}>
-           {posts.length >= 1 ? posts.map(({id, title, body,link}) => { return(
+           {posts.length >= 1 ? posts.map(({id, title, body,link, comment_count}) => { return(
                    <Card id="post" key={id} style={{ width: '18rem' }}>
                        <Card.Body>
                            <Card.Title>{title}</Card.Title>
                            <Card.Text>
                                {body}
                            </Card.Text>
-                           <Card.Link href={link} id={"read"}>Read More</Card.Link>
+                           <Card.Text>
+                               <Card.Link href={link} id={"read"}>Read More</Card.Link>
+                               <CardLink id={"comments-count"}> <LiaComment id={"comments-icon"} className={"m-2"}/>
+                                   {comment_count}</CardLink>
+                           </Card.Text>
                        </Card.Body>
                    </Card>
            );}) : <div className={"spinner"}><Spinner animation={"border"} /></div>}
