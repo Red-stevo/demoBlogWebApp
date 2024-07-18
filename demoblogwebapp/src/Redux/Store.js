123245c55b1e75ad0c500740ca7ed2faa84b8a1f
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-07-18 20:14:52 +0300

    Commit message >>>>> registered the login reducer. <<<<<

--- demoblogwebapp/src/Redux/Store.js
+++ demoblogwebapp/src/Redux/Store.js
@@ -1,11 +1,13 @@
 import {applyMiddleware, combineReducers, compose, createStore} from "redux";
 import {storePostComments, storePostState} from "./Reducers/PostReducer.js";
 import {thunk} from "redux-thunk";
+import {userLoginReducer} from "./Reducers/UserAuthReducer.js";
 
 
 const reducers = combineReducers({
     Posts:storePostState,
     comments:storePostComments,
+    userDetails:userLoginReducer,
 });
 
 // eslint-disable-next-line no-undef
