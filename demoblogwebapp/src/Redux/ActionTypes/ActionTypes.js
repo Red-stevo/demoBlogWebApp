82669bdf9e6662e9543741423e083c91435efeb4
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-07-18 20:07:21 +0300

    Commit message >>>>> added the action creator for user login <<<<<

--- demoblogwebapp/src/Redux/ActionTypes/ActionTypes.js
+++ demoblogwebapp/src/Redux/ActionTypes/ActionTypes.js
@@ -3,4 +3,5 @@
 export const  ActionTypes = {
     FETCH_POST:"FETCH_POST",
     FETCH_COMMENTS:"FETCH_COMMENTS",
+    USER_LOGIN:"USER_LOGIN",
 }
--- demoblogwebapp/src/Redux/Actions/Actions.js
+++ demoblogwebapp/src/Redux/Actions/Actions.js
@@ -1,24 +1,31 @@
 import {resources} from "../utils.js";
 import {ActionTypes} from "../ActionTypes/ActionTypes.js";
 
-
+/*Action creator that returns a promise, that is a function to fetch all the
+* posts from the backend api.*/
 export const fetchPost = () => async (dispatch) => {
         const  response = await resources.get("/posts");
         dispatch({type:ActionTypes.FETCH_POST, payload:response.data});
 }
 
-/*
-export const setPosts = (posts) => {
-    return{
-        type: ActionTypes.FETCH_POST,
-        payload:posts
-    }
-}
-*/
 
-export const fetchComments = (postId) =>
-    async (dispatch) => {
+/*Action creator return a function to fetch comments and filter comments
+*that belong to a specific post.*/
+export const fetchComments = (postId) => async (dispatch) => {
         const response = await resources.get("/comments");
         const data = response.data.filter(comment => comment.postId === parseInt(postId.post));
-        dispatch({type:ActionTypes.FETCH_COMMENTS, payload:data});
-    }
+        dispatch({type: ActionTypes.FETCH_COMMENTS, payload: data});
+}
+
+
+/*Action creator to handle use login, and the store the user's name and the backend response
+* for later use.*/
+export const userLogin = (loginDetails) => async (dispatch) => {
+        const response = await resources.post("/login", loginDetails);
+        const userData = {
+                username:loginDetails.username,
+                userToken:response.data.token,
+                isLoggedIn:response.data.success
+        }
+        dispatch({type:ActionTypes.USER_LOGIN, payload:userData});
+}
