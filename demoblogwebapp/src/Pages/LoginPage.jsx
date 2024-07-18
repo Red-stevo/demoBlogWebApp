613f51b99f955660c147d1b11a0e71f40438b6c6
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-07-18 17:57:20 +0300

    Commit message >>>>> desigining th elogin page <<<<<

--- demoblogwebapp/src/Pages/LoginPage.jsx
+++ demoblogwebapp/src/Pages/LoginPage.jsx
@@ -1,8 +1,23 @@
+import {Button, Form} from "react-bootstrap";
+import "./../Styling/loginPage.css"
+
 const LoginPage = () => {
     return (
-        <>
-            <h1>Login Page</h1>
-        </>
+        <div className={"login-page"}>
+            <Form id={"login-form"}>
+                <Form.Label>Login</Form.Label>
+                <Form.Group className={"p-2"}>
+                    <Form.Label id={"username-id"} htmlFor={"username"}>Username </Form.Label>
+                    <Form.Control id={"username"}/>
+                </Form.Group>
+                <Form.Group className={"p-2"}>
+                    <Form.Label id={"password-label"} htmlFor={"password"}>Password </Form.Label>
+                    <Form.Control id={"password"} />
+                </Form.Group>
+
+                <Button className={"m-2"} id={"login-button"} type={"submit"}>login</Button>
+            </Form>
+        </div>
     );
 }
 
