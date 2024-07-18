5b3131f5ddfce9706ee360a53a2673a68fd3d2a7
Red-stevo <stephen.muiru22@students.dkut.ac.ke>
2024-07-18 19:02:33 +0300

    Commit message >>>>> working on the js data collections. <<<<<

--- demoblogwebapp/src/Pages/LoginPage.jsx
+++ demoblogwebapp/src/Pages/LoginPage.jsx
@@ -1,18 +1,30 @@
 import {Button, Form} from "react-bootstrap";
 import "./../Styling/loginPage.css"
+import {useState} from "react";
 
 const LoginPage = () => {
+    const [loginDetails, setLoginDetails] = useState({
+        username:"",
+        password:"",
+    });
+
+
     return (
         <div className={"login-page"}>
             <Form id={"login-form"}>
                 <Form.Label>Login</Form.Label>
                 <Form.Group className={"p-2"}>
                     <Form.Label id={"username-label"} htmlFor={"username"}>Username </Form.Label>
-                    <Form.Control id={"username"}/>
+                    <Form.Control id={"username"} value={loginDetails.username}
+                                  onChange={(e) =>
+                                      setLoginDetails({...loginDetails, username: e.target.value})}/>
                 </Form.Group>
                 <Form.Group className={"p-2"}>
                     <Form.Label id={"password-label"} htmlFor={"password"}>Password </Form.Label>
-                    <Form.Control id={"password"} />
+                    <Form.Control type={"password"} id={"password"} value={loginDetails.password}
+                                  onChange={(e) =>
+                                      setLoginDetails({...loginDetails, password: e.target.value})}/>
+                    <div> </div>
                 </Form.Group>
                 <Button className={"m-2"} id={"login-button"} type={"submit"}>login</Button>
             </Form>
